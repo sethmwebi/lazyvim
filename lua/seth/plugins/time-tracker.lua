c1f1c8fe8cd2f0608b03a4b11f50c2d0a77aaee8
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-07 08:20:21 +0300

    added time-tracker

---
+++ lua/seth/plugins/time-tracker.lua
@@ -0,0 +1,10 @@
+return {
+	"3rd/time-tracker.nvim",
+	dependencies = { "3rd/sqlite.nvim" },
+	event = "VeryLazy",
+	opts = {
+		data_file = vim.fn.stdpath("data") .. "/time-tracker.db",
+		tracking_events = { "BufEnter", "BufWinEnter", "CursorMoved", "CursorMovedI", "WinScrolled" },
+		tracking_timeout_seconds = 5 * 60, -- 5 minutes
+	},
+}
